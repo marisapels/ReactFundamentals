ed35b660871a670d5467dc08a309dced2154799f
Maris Apels <maris_apels@elkogroup.com>
2018-01-31 13:31:42 +0200

    First project contact created- added some stuff for Apel's Lab :)

---
+++ Contacts/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './apelslaboratory.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to My Laboratory</h1>
+        </header>
+        <p className="App-intro">
+            .NET Development & Operations
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
