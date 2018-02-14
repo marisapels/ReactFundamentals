9f532a71f39b1f70dce4815cd046617e05335ce5
Maris Apels <maris_apels@elkogroup.com>
2018-02-14 15:58:25 +0200

    started to work on router, not fiinished

--- contacts/src/CreateContact.js
+++ contacts/src/CreateContact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class CreateContact extends Component {
     render() {
         return (
-            <div>CreateContact!</div>
+            <div>CreateContact!</div> 
         )
     }
 }
